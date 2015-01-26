53ca519b8354ccb12bf30395ab56eb1e4f962f23
KingLei <linxli3@gmail.com>
2015-01-26 15:28:26 +0800

    create a constructor of the class WebGLWin initailize Scene camera geometry and material

--- webGLWin.js
+++ webGLWin.js
@@ -1,3 +1,17 @@
-WebGLWin = function ( canvas, camera, mesh ){
+WebGLWin = function ( canvas, camera, mesh, material ){
     this.canvas = canvas || { };
+    
+    this.scene = new THREE.Scene();
+    
+    this.camera = ( camera !== undefined ) ? camera :
+                                 new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 
+                                                                                                 0.1,  1000);
+    this.camera.position.set (0, 0, 10);
+    
+    this.geometry = ( mesh !== undefined ) ? mesh : 
+                                     new THREE.OctahedronGeometry( 1, 2);
+                                     
+    this.material = ( material !== undefined ) ? material : 
+                                   new THREE.MeshBasicMaterial( {color : 0x00ff00});
+    this.material.wirefreame = true;
 }
