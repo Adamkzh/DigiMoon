b0b71bd62ac35aaba00f6479a55a79594e1aafc2
KingLei <linxli3@gmail.com>
2015-01-26 16:06:16 +0800

    add a draw function initailize WebGLWin with an octahedron

--- digiMoon.js
+++ digiMoon.js
@@ -1,11 +1,13 @@
-Window.onload = function() {
-    
+window.onload = function() {
     // initialize canvas size
-    var canvas = document.getElementById("webMoon") || { };
+    var canvas = document.getElementById( "WebMoon" );
     //canvas.width  = window.innerWidth;
     //canvas.height = window.innerHeight;
+  
+    var DigiMoon = new WebGLWin( canvas );
+     //DigiMoon.canvas.width = window.innerWidth;
+	//DigiMoon.canvas.height = window.innerHeight; 
     
-    
-    var DigiMoon = new WebGLWin( canvas);
+    DigiMoon.draw();
     
 }
--- webGLWin.js
+++ webGLWin.js
@@ -1,17 +1,36 @@
-WebGLWin = function ( canvas, camera, mesh, material ){
+WebGLWin = function ( canvas, camera, geometry, material ){
+    
     this.canvas = canvas || { };
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
     
     this.scene = new THREE.Scene();
     
     this.camera = ( camera !== undefined ) ? camera :
                                  new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 
                                                                                                  0.1,  1000);
-    this.camera.position.set (0, 0, 10);
+    this.camera.position.set ( 0, 0, 5 );
     
-    this.geometry = ( mesh !== undefined ) ? mesh : 
-                                     new THREE.OctahedronGeometry( 1, 2);
+    this.geometry = ( geometry !== undefined ) ? geometry : 
+                                     new THREE.OctahedronGeometry( 1, 2 );
                                      
     this.material = ( material !== undefined ) ? material : 
-                                   new THREE.MeshBasicMaterial( {color : 0x00ff00});
-    this.material.wirefreame = true;
+                                    new THREE.MeshBasicMaterial( {color : 0x00ff00} );
+    this.material.wireframe = true;
+    
+   this.models =  new THREE.Mesh( this.geometry, this.material );
+   
+    this.scene.add( this.models );
+    
+    this.renderer = new THREE.WebGLRenderer( {canvas: canvas} ) ;
+}
+
+WebGLWin.prototype = {
+    
+    constructor : WebGLWin, 
+    
+    draw : function() {
+       this.renderer.render( this.scene, this.camera );
+   },
+       
 }
