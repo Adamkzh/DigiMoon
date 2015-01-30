a469d0af50f4ebd946bc904db0a332ae011d0018
KingLei <linxli3@gmail.com>
2015-01-30 15:09:16 +0800

    add mouse wheel scale function

--- digiMoon.js
+++ digiMoon.js
@@ -1,13 +1,54 @@
+// initialize canvas size
+var canvas = document.getElementById( "WebMoon" );
+
+var scaleLevel = 5;
+
+var DigiMoon = new WebGLWin( canvas );
+
+if( canvas.addEventListener ){
+	//IE9, Chrome, Safari, Opera
+	canvas.addEventListener("mousewheel",
+							MouseWheelHandler,
+							false);
+	// Firefox
+	canvas.addEventListener("DOMMouseScroll",
+							MouseWheelHandler,
+							false);
+}
+// IE 6/7/8
+//else canvas.attachEvent("onmousewheel", MouseWheelHandler);
+
 window.onload = function() {
-    // initialize canvas size
-    var canvas = document.getElementById( "WebMoon" );
-    //canvas.width  = window.innerWidth;
-    //canvas.height = window.innerHeight;
-  
-    var DigiMoon = new WebGLWin( canvas );
-     //DigiMoon.canvas.width = window.innerWidth;
-	//DigiMoon.canvas.height = window.innerHeight; 
+    
+    
+    PLYFile = './CHE1Mesh4V2.ply';
+    //loader = new THREE.PLYLoader();
+    
+    //geometry = loader.load(PLYFile, function( event ){ return event; });
+    //alert(geometry);
+    
+    //alert(DigiMoon.geometry.id);
+    //DigiMoon.loadPLY(PLYFile);
+    //alert(DigiMoon.geometry.id);
     
     DigiMoon.draw();
     
-}
+};
+
+function MouseWheelHandler(event){
+	
+	var e = event || window.event;
+	
+	var scale = e.wheelDelta || e.detail;
+	if (scale >= 0 && scaleLevel >= 1 ){
+		scaleLevel -=0.3;
+	}
+	else if (scale < 0 && scaleLevel < 40){
+		scaleLevel +=0.3;
+	};
+
+	DigiMoon.camera.position.set(0,0,scaleLevel);
+	DigiMoon.draw();
+	console.log(scaleLevel);
+
+};
--- webGLWin.js
+++ webGLWin.js
@@ -1,4 +1,4 @@
-WebGLWin = function ( canvas, camera, geometry, material ){
+WebGLWin = function ( canvas, geometry, camera, material ){
     
     this.canvas = canvas || { };
     this.canvas.width = window.innerWidth;
@@ -7,22 +7,25 @@ WebGLWin = function ( canvas, camera, geometry, material ){
     this.scene = new THREE.Scene();
     
     this.camera = ( camera !== undefined ) ? camera :
-                                 new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 
-                                                                                                 0.1,  1000);
+                  new THREE.PerspectiveCamera(75,
+                                              window.innerWidth/window.innerHeight,
+                                              0.1,  1000);
     this.camera.position.set ( 0, 0, 5 );
     
     this.geometry = ( geometry !== undefined ) ? geometry : 
-                                     new THREE.OctahedronGeometry( 1, 2 );
+                    new THREE.OctahedronGeometry( 1, 4 );
                                      
     this.material = ( material !== undefined ) ? material : 
-                                    new THREE.MeshBasicMaterial( {color : 0x00ff00} );
+                    new THREE.MeshBasicMaterial( {color : 0x00ff00} );
     this.material.wireframe = true;
     
-   this.models =  new THREE.Mesh( this.geometry, this.material );
+    this.models =  new THREE.Mesh( this.geometry, this.material );
    
     this.scene.add( this.models );
     
     this.renderer = new THREE.WebGLRenderer( {canvas: canvas} ) ;
+    
+    this.loader = new THREE.PLYLoader();
 }
 
 WebGLWin.prototype = {
@@ -32,5 +35,14 @@ WebGLWin.prototype = {
     draw : function() {
        this.renderer.render( this.scene, this.camera );
    },
+   
+   loadPLY : function( PLYFile ){
+       var scope = this;
+       scope.loader.load( PLYFile, 
+           function(scope, event){this.models.geometry = event;
+                                  this.models.material.color = 0xff0000;
+                                  this.scene.add(this.models);}, scope );
+       },
+       
        
 }
